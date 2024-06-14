26514a340b433c3ded133e608789625494a4783c
nguansak <nguansak@gmail.com>
2024-06-14 06:06:25 +0000

    Initial Project

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>Welcome to My Space!</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  // Initialize a count variable to 0
+  let count: number = 0 
+
+  // Define a function to increment the count
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
