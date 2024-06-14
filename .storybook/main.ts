009a72432045c12b42aefd2074e913229a98cbfd
nguansak <nguansak@gmail.com>
2024-06-14 16:48:22 +0000

    chore: downgrade Storybook to version 8.1.9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,8 @@
 import type { StorybookConfig } from "@storybook/svelte-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
   addons: [
-    "@storybook/addon-svelte-csf",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
